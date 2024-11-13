170d3cfacaf49133173639ad3fd715ccf839090f
Daniel Malede <danielmalede@gmail.com>
2024-11-13 09:52:21 +0200

    First commit

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,15 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		// Replace this comment with the rest of your code
+
+		String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		Double bill = Double.parseDouble(args[3]);
+
+		Double billingSplit = bill / 3;
+		billingSplit = Math.ceil(billingSplit);
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + billingSplit + " Shekels each.");
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,20 @@
 // Computes the future value of a saving investment.
 public class FVCalc {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		// Replace this comment with your code
+		String currentValue = args[0];
+		String rate = args[1];
+		String years = args[2];
+		String result = "" + (Double.parseDouble(currentValue)
+				* Math.pow(1 + Double.parseDouble(rate) / 100, Integer.parseInt(years)));
+		double intRate = Double.parseDouble(args[1]);
+		double doubleNumber = Double.parseDouble(result);
+		double roundedNumber =Math.floor(doubleNumber);
+		int finalResult = (int) roundedNumber;
+		if (args != null) {
+			System.out.println(
+					"After " + years + " years, a $" + currentValue + " saved at "  + intRate + "%" + " will yield $"
+							+ finalResult);
+		}
 	}
 }
